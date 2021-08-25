ef5862ff5bbb73c9314f22d117775604a419d0d1
Francis Albores <mrthemetribe@gmail.com>
2021-08-25 15:56:41 +0800

    useContext run perfectly for app, product and productList Files

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import Header from "./Header";
 import Products from "./Products";
 import ProductList from "./ProductList";
 import { useState } from "react";
+import { ProductContext } from "./ProductContext";
 
-const setProduct = (name, quantity) => {
-  const todos = JSON.parse(localStorage.getItem("Products"))
-  todos.push({name, quantity});
-  localStorage.setItem("Products", JSON.stringify(todos));
-}
 
-const providerValue = {
-  products : JSON.parse(localStorage.getItem("Products")),
-  setProducts : (name, quantity) => setProduct(name, quantity)
-}
 
 function App() {
-  const [product, setProduct] =  useState([]) 
+
+  const [products, setProducts] = useState([]);
+  
+  const providerValue = {
+    products,
+    setProducts: (name, quantity) => {
+      setProducts([
+        ...products,
+        {name, quantity}
+      ])
+    },
+  };
   return (
     <div className="App">
-       <ProductContext.Provider value={providerValue} >
-      <Header />
-      <Products />
-      <ProductList />
+      <ProductContext.Provider value={providerValue}>
+        <Header />
+        <Products />
+        <ProductList />
       </ProductContext.Provider>
     </div>
   );
